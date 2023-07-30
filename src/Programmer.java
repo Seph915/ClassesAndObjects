a82741896c646198a8581713025acf1314867dea
fabio <fabiocostanza91@gmail.com>
2023-07-30 21:25:34 +0200

    exercise 16

---
+++ src/Programmer.java
@@ -0,0 +1,18 @@
+public class Programmer {
+    public static void main(String[] args) {
+
+    }
+    String name = "Rosa";
+    int age = 18;
+    boolean wearGlasses = true;
+
+    void drinkCoffee(){
+        System.out.println("Espresso is the secret!");
+    }
+    void printDetails(){
+        System.out.println(name + " is a " + age +"-yo programmer");
+    }
+    void hasGlasses(){
+        System.out.println("Is " + name + " wearing glasses? " + wearGlasses);
+    }
+}
---
+++ src/TestProgrammer.java
@@ -0,0 +1,14 @@
+public class TestProgrammer {
+    public static void main(String[] args) {
+
+        Programmer programmer1 = new Programmer();
+        Programmer programmer2 = new Programmer();
+
+        programmer1.drinkCoffee();
+        programmer1.printDetails();
+
+        programmer2.printDetails();
+        programmer2.hasGlasses();
+    }
+}
+
